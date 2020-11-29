0409981f7b00e2eeb5a066849db4562db9f73994
receyuki <receyuki@gmail.com>
2020-11-30 03:43:33 +1100

    Initial Commit

---
+++ Icon Replacer/Icon_ReplacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Icon_ReplacerApp.swift
+//  Icon Replacer
+//
+//  Created by ReceYuki on 30/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Icon_ReplacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Icon ReplacerTests/Icon_ReplacerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Icon_ReplacerTests.swift
+//  Icon ReplacerTests
+//
+//  Created by ReceYuki on 30/11/20.
+//
+
+import XCTest
+@testable import Icon_Replacer
+
+class Icon_ReplacerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
